99199e5e1e3c3f9520a8d6fbf5061ac30d15105c
Peter Kang <kkang21@amherst.edu>
2020-11-02 10:54:19 -0500

    wrangle code

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
+DATASET <- readr::read_csv("allCongressDataPublishV2.csv") %>%
+  janitor::clean_names() %>%
+  select(last_name, first_name,  state_dist, number_terms,
+         age, party, pass_prcnt, prcnt_ba, prcnt_hs, prcnt_white_all)
+
 usethis::use_data(DATASET, overwrite = TRUE)
